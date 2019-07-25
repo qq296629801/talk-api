9fc702615a997dd6f71b486d646226eb3b14fb1c
mjy <296629801@qq.com>
2019-07-25 10:54:31 +0800

    拆分模块

--- src/main/java/cn/ymsys/api/common/request/UserRequest.java
+++
@@ -1,9 +0,0 @@
-package cn.ymsys.api.common.request;
-
-import cn.ymsys.api.common.util.PagerUtil;
-import lombok.Data;
-
-@Data
-public class UserRequest extends PagerUtil {
-    private Integer id;
-}
