963b194411f37342dd9bb39423005d6b7d330950
mjy <296629801@qq.com>
2019-08-07 21:43:48 +0800

    1

--- user/src/main/java/cn/ymsys/api/common/websocket/handler/GroupMessageRequestHandler.java
+++ user/src/main/java/cn/ymsys/api/common/websocket/handler/GroupMessageRequestHandler.java
@@ -1,11 +1,13 @@
 package cn.ymsys.api.common.websocket.handler;
 
+import cn.ymsys.api.common.request.ChatRequest;
 import cn.ymsys.api.common.request.GroupMsgRequest;
 import cn.ymsys.api.common.util.SpringContextUtil;
 import cn.ymsys.api.common.websocket.protocol.request.GroupMessageRequestPacket;
 import cn.ymsys.api.common.websocket.protocol.response.GroupMessageResponsePacket;
 import cn.ymsys.api.common.websocket.session.Session;
 import cn.ymsys.api.common.websocket.util.SessionUtil;
+import cn.ymsys.api.service.ChatService;
 import cn.ymsys.api.service.GroupMessageService;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
@@ -27,17 +29,28 @@ public class GroupMessageRequestHandler extends SimpleChannelInboundHandler<Grou
         String groupId = msg.getToGroupId();
         String message = msg.getMessage();
         Session session = SessionUtil.getSession(ctx.channel());
+        String userId = session.getUserId();
+        Integer msgType = msg.getMsgType();
 
-        // 构造参数
-        GroupMsgRequest vo = new GroupMsgRequest();
-        vo.setMsgType(msg.getMsgType());
-        vo.setGroupId(msg.getToGroupId());
-        vo.setMessage(msg.getMessage());
-        vo.setSendUid(session.getUserId());
+        // 构造聊天记录参数
+        GroupMsgRequest groupMsgReq = new GroupMsgRequest();
+        groupMsgReq.setMsgType(msgType);
+        groupMsgReq.setGroupId(groupId);
+        groupMsgReq.setMessage(message);
+        groupMsgReq.setSendUid(userId);
+
+        // 构造聊天列表参数
+        ChatRequest chatReq = new ChatRequest();
+        chatReq.setUserId(userId);
+        chatReq.setChatId(groupId);
+        chatReq.setChatType(msgType);
+        chatReq.setDesc(message);
 
         // 保存聊天记录
         GroupMessageService groupMessageService = SpringContextUtil.getBean(GroupMessageService.class);
-        groupMessageService.create(vo);
+        ChatService chatService = SpringContextUtil.getBean(ChatService.class);
+        groupMessageService.create(groupMsgReq);
+        chatService.openChat(chatReq);
 
         // 构造群聊消息的响应数据包
         GroupMessageResponsePacket groupMessageResponsePacket = new GroupMessageResponsePacket();
--- user/src/main/java/cn/ymsys/api/service/ChatService.java
+++ user/src/main/java/cn/ymsys/api/service/ChatService.java
@@ -1,6 +1,7 @@
 package cn.ymsys.api.service;
 
 import cn.ymsys.api.common.request.ChatRequest;
+import cn.ymsys.api.common.util.DataUtil;
 import cn.ymsys.api.common.util.PagerUtil;
 import cn.ymsys.api.orm.mapper.ChatMapper;
 import cn.ymsys.api.orm.model.Chat;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -33,17 +35,47 @@ public class ChatService {
         return chats;
     }
 
-    public Chat openTalk(ChatRequest vo) {
-        Chat chat = new Chat();
-        chat.setChatId(vo.getChatId());
-        chat.setChatType(vo.getChatType());
-        chat.setDesc(vo.getDesc());
-        chatMapper.insertSelective(chat);
+    public boolean checkChat(ChatRequest vo) {
+        ChatExample example = new ChatExample();
+        ChatExample.Criteria criteria = example.createCriteria();
+        criteria.andUserIdEqualTo(vo.getUserId());
+        criteria.andChatTypeEqualTo(vo.getChatType());
+        criteria.andChatIdEqualTo(vo.getChatId());
+        criteria.andStatusEqualTo(0);
+        return chatMapper.countByExample(example) > 0 ? true : false;
+    }
+
+    public Chat queryByPara(ChatRequest vo) {
+        ChatExample example = new ChatExample();
+        ChatExample.Criteria criteria = example.createCriteria();
+        criteria.andUserIdEqualTo(vo.getUserId());
+        criteria.andChatTypeEqualTo(vo.getChatType());
+        criteria.andChatIdEqualTo(vo.getChatId());
+        criteria.andStatusEqualTo(0);
+        return chatMapper.selectByExample(example).get(0);
+    }
+
+
+    public Chat openChat(ChatRequest vo) {
+        Chat chat = queryByPara(vo);
+        if (DataUtil.isNull(chat)) {
+            chat = new Chat();
+            chat.setChatId(vo.getChatId());
+            chat.setChatType(vo.getChatType());
+            chat.setDesc(vo.getDesc());
+            chat.setLastOpenTime(new Date());
+            chat.setOpenTime(new Date());
+            chatMapper.insertSelective(chat);
+        } else {
+            chat.setDesc(vo.getDesc());
+            chat.setLastOpenTime(new Date());
+            chatMapper.updateByPrimaryKeySelective(chat);
+        }
         return chat;
     }
 
 
-    public int closeTalk(ChatRequest vo) {
+    public int closeChat(ChatRequest vo) {
         return chatMapper.deleteByPrimaryKey(vo.getId());
     }
 
