2f7d95eae425e2f635940591afd06c0ac9374f0b
mjy <296629801@qq.com>
2019-08-07 10:23:25 +0800

    1

---
+++ user/src/main/java/cn/ymsys/api/common/request/GroupMsgRequest.java
@@ -0,0 +1,11 @@
+package cn.ymsys.api.common.request;
+
+import lombok.Data;
+
+@Data
+public class GroupMsgRequest {
+    private String groupId;
+    private String sendUid;
+    private Integer msgType;
+    private String message;
+}
---
+++ user/src/main/java/cn/ymsys/api/service/GroupMessageService.java
@@ -0,0 +1,39 @@
+package cn.ymsys.api.service;
+
+import cn.ymsys.api.common.request.GroupMsgRequest;
+import cn.ymsys.api.orm.mapper.GroupMsgMapper;
+import cn.ymsys.api.orm.model.GroupMsg;
+import cn.ymsys.api.orm.model.GroupMsgExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class GroupMessageService {
+
+    @Autowired
+    private GroupMsgMapper groupMsgMapper;
+
+    public List<GroupMsg> queryGroupMsgs(GroupMsgRequest vo) {
+        GroupMsgExample example = new GroupMsgExample();
+        GroupMsgExample.Criteria criteria = example.createCriteria();
+        criteria.andGroupIdEqualTo(vo.getGroupId());
+        criteria.andSendUidEqualTo(vo.getSendUid());
+
+        return groupMsgMapper.selectByExample(example);
+    }
+
+    public GroupMsg create(GroupMsgRequest vo) {
+        GroupMsg gm = new GroupMsg();
+        gm.setGroupId(vo.getGroupId());
+        gm.setMsgContext(vo.getMessage());
+        gm.setMsgType(vo.getMsgType());
+        gm.setSendUid(vo.getSendUid());
+        gm.setLastOperTime(new Date());
+        gm.setOperTime(new Date());
+        groupMsgMapper.insertSelective(gm);
+        return gm;
+    }
+}
