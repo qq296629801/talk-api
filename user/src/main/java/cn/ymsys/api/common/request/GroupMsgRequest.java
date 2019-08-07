a8cad7f591aff57aaa455bdf478c043b4215481f
mjy <296629801@qq.com>
2019-08-07 20:31:27 +0800

    1

--- user/src/main/java/cn/ymsys/api/common/request/GroupMsgRequest.java
+++ user/src/main/java/cn/ymsys/api/common/request/GroupMsgRequest.java
@@ -1,9 +1,10 @@
 package cn.ymsys.api.common.request;
 
+import cn.ymsys.api.common.util.PagerUtil;
 import lombok.Data;
 
 @Data
-public class GroupMsgRequest {
+public class GroupMsgRequest extends PagerUtil {
     private String groupId;
     private String sendUid;
     private Integer msgType;
--- user/src/main/java/cn/ymsys/api/service/GroupMessageService.java
+++ user/src/main/java/cn/ymsys/api/service/GroupMessageService.java
@@ -1,15 +1,20 @@
 package cn.ymsys.api.service;
 
 import cn.ymsys.api.common.request.GroupMsgRequest;
+import cn.ymsys.api.common.util.PagerUtil;
 import cn.ymsys.api.orm.mapper.GroupMsgMapper;
 import cn.ymsys.api.orm.model.GroupMsg;
 import cn.ymsys.api.orm.model.GroupMsgExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * @author mjy
+ */
 @Service
 public class GroupMessageService {
 
@@ -17,10 +22,17 @@ public class GroupMessageService {
     private GroupMsgMapper groupMsgMapper;
 
     public List<GroupMsg> queryGroupMsgs(GroupMsgRequest vo) {
-        GroupMsgExample example = new GroupMsgExample();
-        GroupMsgExample.Criteria criteria = example.createCriteria();
-        criteria.andGroupIdEqualTo(vo.getGroupId());
-        return groupMsgMapper.selectByExample(example);
+        List<GroupMsg> groupMsgs = new ArrayList<>();
+        try {
+            PagerUtil.startPage(vo);
+            GroupMsgExample example = new GroupMsgExample();
+            GroupMsgExample.Criteria criteria = example.createCriteria();
+            criteria.andGroupIdEqualTo(vo.getGroupId());
+            groupMsgs = groupMsgMapper.selectByExample(example);
+        } catch (Exception e) {
+            PagerUtil.clearPage(vo);
+        }
+        return groupMsgs;
     }
 
     public GroupMsg create(GroupMsgRequest vo) {
